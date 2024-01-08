4454fb9562122fcf21e5b1281e20fd163b6c752c
Garo Yeriazarian <garo.yeriazarian@accenture.com>
2024-01-08 14:09:23 -0600

    Prettify

--- src/main.ts
+++ src/main.ts
@@ -6,8 +6,6 @@ import * as core from '@actions/core'
  */
 export async function run(): Promise<void> {
   try {
-    
-
   } catch (error) {
     // Fail the workflow run if an error occurs
     if (error instanceof Error) core.setFailed(error.message)
