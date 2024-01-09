884bb5e5a20ead2e732b5f0ffe11b2461811ca5c
Garo Yeriazarian <garo.yeriazarian@accenture.com>
2024-01-08 23:39:06 -0600

    Add error handler

--- src/util.ts
+++ src/util.ts
@@ -1,13 +1,24 @@
-type Jsonable = string | number | boolean | null | undefined | readonly Jsonable[] | { readonly [key: string]: Jsonable } | { toJSON(): Jsonable }
+export type Jsonable =
+  | string
+  | number
+  | boolean
+  | null
+  | undefined
+  | readonly Jsonable[]
+  | { readonly [key: string]: Jsonable }
+  | { toJSON(): Jsonable }
 
 export class ActionError extends Error {
-    public readonly context?: Jsonable
+  public readonly context?: Jsonable
 
-    constructor(message: string, options: { cause?: Error, context?: Jsonable } = {}) {
-        const { cause, context } = options
-        super(message, {cause})
-        
-        this.name = this.constructor.name
-        this.context = context
-    }
+  constructor(
+    message: string,
+    options: { cause?: Error; context?: Jsonable } = {}
+  ) {
+    const { cause, context } = options
+    super(message, { cause })
+
+    this.name = this.constructor.name
+    this.context = context
+  }
 }
