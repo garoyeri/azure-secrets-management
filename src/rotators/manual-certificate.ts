5d52a8c025906a626dbbef0495f79f02ba8ec160
Garo Yeriazarian <garo.yeriazarian@accenture.com>
2024-01-10 11:20:59 -0600

    Add rotator for manual certificate

---
+++ src/rotators/manual-certificate.ts
@@ -0,0 +1,31 @@
+import { ManagedResource } from 'src/configuration-file'
+import { Rotator } from './abstract-rotator'
+import { RotationResult } from './shared'
+import { OperationSettings } from 'src/operation-settings'
+import { ImportCertificate } from 'src/key-vault'
+
+export class ManualCertificateRotator extends Rotator {
+  constructor(settings: OperationSettings) {
+    super('manual/certificate', 'certificate', settings)
+  }
+
+  async PerformRotation(
+    resource: ManagedResource,
+    secretName: string
+  ): Promise<RotationResult> {
+    const pfxBuffer = resource.decodeBase64 ? Buffer.from(this.settings.secretValue1, 'base64')
+        : Buffer.from(this.settings.secretValue1)
+
+    const result = await ImportCertificate(
+        resource.keyVault,
+        this.settings.credential,
+        secretName,
+        pfxBuffer.valueOf()
+      )
+  
+      return new RotationResult(resource.name, true, '', {
+        id: result.properties.id,
+        expiration: result.properties.expiresOn
+      })  
+  }
+}
